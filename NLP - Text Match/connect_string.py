984b0ca0e694bdb02436c6cefa2e9f2d8495ecbd
Navjot Singh <nsingh1@glgroup.com>
2018-10-07 12:00:55 +0530

    Added separate connection file for encription and reusability

---
+++ NLP - Text Match/connect_string.py
@@ -0,0 +1,9 @@
+## Connection String 
+
+DATABASE = {
+    # --------------------
+    'hostname': '***.**.*.**',  # your DB server name
+    # --------------------
+    'username': '****',          # Username
+    'password': '******'      # Password
+}
